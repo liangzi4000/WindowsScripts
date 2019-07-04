00d88e5e39bbec95ec8af7f1d4d6a8c4c915d0c8
Bruce Ye <brucesgp@gmail.com>
2019-07-04 16:53:50 +0800

    .

--- 04. Admin.Shutdown PC at specified time with prompt.bat
+++ 04. Admin.Shutdown PC at specified time with prompt.bat
@@ -3,3 +3,6 @@ at 5:30 shutdown -s -t 60 -c "System will be shutdown in 60 seconds"
 
 rem shutdown remote PC
 shutdown /s /m \\PC-Name
+
+rem force reboot in 30 seconds, /s cannot use together with /f.
+shutdown /f /r /t 30
