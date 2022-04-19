716a3f65016be5ad7c1fb164529edc737b7b3cd0
Bruce Ye <ye_ping_yang@medinno.com>
2022-04-19 20:54:37 +0800

    Add nuget scripts

---
+++ nuget/01. Add package to feed.bat
@@ -0,0 +1,22 @@
+rem "C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.Core.1.0.29.nupkg" -source "D:\Bruce\NugetLocalServer"
+rem "C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.Core.1.0.29.snupkg" -source "D:\Bruce\NugetLocalServer"
+rem "C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Framework\NugetPackages\NugetPackages\Medisys.Database.EfCore\bin\Debug\Medisys.Database.EfCore.1.0.54.nupkg" -source "D:\Bruce\NugetLocalServer"
+
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.HtmlToPdf.1.0.1.nupkg" -source "D:\Bruce\NugetLocalServer"
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.HtmlToPdf.Type.1.0.0.nupkg" -source "D:\Bruce\NugetLocalServer"
+
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.File.1.0.15.nupkg" -source "D:\Bruce\NugetLocalServer"
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.File.Types.1.0.3.nupkg" -source "D:\Bruce\NugetLocalServer"
+
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.UserManagement.1.0.49.nupkg" -source "D:\Bruce\NugetLocalServer"
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.UserManagement.Types.1.0.49.nupkg" -source "D:\Bruce\NugetLocalServer"
+
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.Core.1.0.48.nupkg" -source "D:\Bruce\NugetLocalServer"
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.Core.Types.1.0.48.nupkg" -source "D:\Bruce\NugetLocalServer"
+
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.Audit.1.0.0.nupkg" -source "D:\Bruce\NugetLocalServer"
+"C:\Program Files (x86)\NuGet\nuget.exe" push "D:\Bruce\TFS\Output\Medisys.Modules.Audit.Types.1.0.0.nupkg" -source "D:\Bruce\NugetLocalServer"
+
+
+
+
---
+++ nuget/02. Clear Debug Package.bat
@@ -0,0 +1,8 @@
+SET PackageName=Medisys.Modules.Audit
+RMDIR /s /q C:\Users\bruceye.MEDINNO\.nuget\packages\%PackageName%
+del D:\Bruce\NugetLocalServer\%PackageName%*.nupkg
+
+SET PackageName=Medisys.Modules.Audit.Types
+RMDIR /s /q C:\Users\bruceye.MEDINNO\.nuget\packages\%PackageName%
+del D:\Bruce\NugetLocalServer\%PackageName%*.nupkg
+
