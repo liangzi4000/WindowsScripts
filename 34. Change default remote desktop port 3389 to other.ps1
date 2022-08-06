5d7d6ad4d3eceb5df30649e2fbca83614e287247
Bruce Ye <yepingyang@yahoo.com>
2022-08-06 12:42:13 +0800

    Add script 34 and 35

---
+++ 34. Change default remote desktop port 3389 to other.ps1
@@ -0,0 +1,4 @@
+$portvalue = 5566
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber" -Value $portvalue
+New-NetFirewallRule -DisplayName 'RDPPORTLatest-TCP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $portvalue
+New-NetFirewallRule -DisplayName 'RDPPORTLatest-UDP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol UDP -LocalPort $portvalue
